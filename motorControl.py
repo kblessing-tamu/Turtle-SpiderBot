c87e8b7a3383cf4a1fce051fe994609b3eea2e94
Emmanuel Oyervides <emmy0021@gmail.com>
2020-05-07 14:21:37 -0500

    motor control

--- motorControl.py
+++ motorControl.py
@@ -1,113 +1,130 @@
-import RPi.GPIO as GPIO          
+import RPi.GPIO as GPIO   
+import sys       
 from time import sleep
+def main(argv):
+    try:
+        opts, args = getopt.getopt(argv,"hi:o:",["mode=","time="])
+    except getopt.GetoptError:
+        sys.exit(2)
 
-in1 = 24
-in2 = 23
-en = 25
-
-en2 = 17
-in3 = 22
-in4 = 27
-
-temp1=1
-
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(in1,GPIO.OUT)
-GPIO.setup(in2,GPIO.OUT)
-GPIO.setup(en,GPIO.OUT)
-
-
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(in3,GPIO.OUT)
-GPIO.setup(in4,GPIO.OUT)
-GPIO.setup(en2,GPIO.OUT)
-
-GPIO.output(in1,GPIO.LOW)
-GPIO.output(in2,GPIO.LOW)
-GPIO.output(in3,GPIO.LOW)
-GPIO.output(in4,GPIO.LOW)
-p=GPIO.PWM(en,1000)
-p1 =GPIO.PWM(en2,1000)
-
-p.start(25)
-p1.start(25)
-print("\n")
-print("The default speed & direction of motor is LOW & Forward.....")
-print("r-run s-stop f-forward b-backward l-low m-medium h-high e-exit")
-print("\n")    
-
-while(1):
-
-    x=raw_input()
+    for opt, arg in opts:
+      if opt == '-h':
+        print 'test.py -m <mode> -t <outputfile>'
+        sys.exit()
+      elif opt in ("-m", "--mode"):
+        mode = arg
+      elif opt in ("-t", "--time"):
+        time = arg
     
-    if x=='r':
-        print("run")
-        if(temp1==1):
-         GPIO.output(in1,GPIO.HIGH)
-         GPIO.output(in2,GPIO.LOW)
-         GPIO.output(in3,GPIO.HIGH)
-         GPIO.output(in4,GPIO.LOW)
-         print("forward")
-         x='z'
+    print 'mode file is "', mode
+    print 'time file is "', time
+    in1 = 24
+    in2 = 23
+    en = 25
+
+    en2 = 17
+    in3 = 22
+    in4 = 27
+
+    temp1=1
+
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(in1,GPIO.OUT)
+    GPIO.setup(in2,GPIO.OUT)
+    GPIO.setup(en,GPIO.OUT)
+
+
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(in3,GPIO.OUT)
+    GPIO.setup(in4,GPIO.OUT)
+    GPIO.setup(en2,GPIO.OUT)
+
+    GPIO.output(in1,GPIO.LOW)
+    GPIO.output(in2,GPIO.LOW)
+    GPIO.output(in3,GPIO.LOW)
+    GPIO.output(in4,GPIO.LOW)
+    p=GPIO.PWM(en,1000)
+    p1 =GPIO.PWM(en2,1000)
+
+    p.start(25)
+    p1.start(25)
+    print("\n")
+    print("The default speed & direction of motor is LOW & Forward.....")
+    print("r-run s-stop f-forward b-backward l-low m-medium h-high e-exit")
+    print("\n")    
+
+    while(1):
+
+        x=raw_input()
+        
+        if x=='r':
+            print("run")
+            if(temp1==1):
+            GPIO.output(in1,GPIO.HIGH)
+            GPIO.output(in2,GPIO.LOW)
+            GPIO.output(in3,GPIO.HIGH)
+            GPIO.output(in4,GPIO.LOW)
+            print("forward")
+            x='z'
+            else:
+            GPIO.output(in1,GPIO.LOW)
+            GPIO.output(in2,GPIO.HIGH)
+            GPIO.output(in3,GPIO.LOW)
+            GPIO.output(in4,GPIO.HIGH)
+            print("backward")
+            x='z'
+
+
+        elif x=='s':
+            print("stop")
+            GPIO.output(in1,GPIO.LOW)
+            GPIO.output(in2,GPIO.LOW)
+            GPIO.output(in3,GPIO.LOW)
+            GPIO.output(in4,GPIO.LOW)
+            x='z'
+
+        elif x=='f':
+            print("forward")
+            GPIO.output(in1,GPIO.HIGH)
+            GPIO.output(in2,GPIO.LOW)
+            GPIO.output(in3,GPIO.HIGH)
+            GPIO.output(in4,GPIO.LOW)
+            temp1=1
+            x='z'
+
+        elif x=='b':
+            print("backward")
+            GPIO.output(in1,GPIO.LOW)
+            GPIO.output(in2,GPIO.HIGH)
+            GPIO.output(in3,GPIO.LOW)
+            GPIO.output(in4,GPIO.HIGH)
+            temp1=0
+            x='z'
+
+        elif x=='l':
+            print("low")
+            p.ChangeDutyCycle(25)
+            p1.ChangeDutyCycle(25)
+            x='z'
+
+        elif x=='m':
+            print("medium")
+            p.ChangeDutyCycle(50)
+            p1.ChangeDutyCycle(50)
+            x='z'
+
+        elif x=='h':
+            print("high")
+            p.ChangeDutyCycle(100)
+            p1.ChangeDutyCycle(100)
+            x='z'
+        
+        
+        elif x=='e':
+            GPIO.cleanup()
+            print("GPIO Clean up")
+            break
+        
         else:
-         GPIO.output(in1,GPIO.LOW)
-         GPIO.output(in2,GPIO.HIGH)
-         GPIO.output(in3,GPIO.LOW)
-         GPIO.output(in4,GPIO.HIGH)
-         print("backward")
-         x='z'
-
-
-    elif x=='s':
-        print("stop")
-        GPIO.output(in1,GPIO.LOW)
-        GPIO.output(in2,GPIO.LOW)
-        GPIO.output(in3,GPIO.LOW)
-        GPIO.output(in4,GPIO.LOW)
-        x='z'
-
-    elif x=='f':
-        print("forward")
-        GPIO.output(in1,GPIO.HIGH)
-        GPIO.output(in2,GPIO.LOW)
-        GPIO.output(in3,GPIO.HIGH)
-        GPIO.output(in4,GPIO.LOW)
-        temp1=1
-        x='z'
-
-    elif x=='b':
-        print("backward")
-        GPIO.output(in1,GPIO.LOW)
-        GPIO.output(in2,GPIO.HIGH)
-        GPIO.output(in3,GPIO.LOW)
-        GPIO.output(in4,GPIO.HIGH)
-        temp1=0
-        x='z'
-
-    elif x=='l':
-        print("low")
-        p.ChangeDutyCycle(25)
-        p1.ChangeDutyCycle(25)
-        x='z'
-
-    elif x=='m':
-        print("medium")
-        p.ChangeDutyCycle(50)
-        p1.ChangeDutyCycle(50)
-        x='z'
-
-    elif x=='h':
-        print("high")
-        p.ChangeDutyCycle(100)
-        p1.ChangeDutyCycle(100)
-        x='z'
-     
-    
-    elif x=='e':
-        GPIO.cleanup()
-        print("GPIO Clean up")
-        break
-    
-    else:
-        print("<<<  wrong data  >>>")
-        print("please enter the defined data to continue.....")
+            print("<<<  wrong data  >>>")
+            print("please enter the defined data to continue.....")
