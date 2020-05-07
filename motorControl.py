28a7023d8a6d478903aa9edc67137ddc09f4f7e0
Emmanuel Oyervides <emmy0021@gmail.com>
2020-05-07 08:22:48 -0700

    Double motor control

--- motorControl.py
+++ motorControl.py
@@ -7,7 +7,7 @@ en = 25
 
 en2 = 17
 in3 = 22
-in4 27
+in4 = 27
 
 temp1=1
 
@@ -15,19 +15,22 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setup(in1,GPIO.OUT)
 GPIO.setup(in2,GPIO.OUT)
 GPIO.setup(en,GPIO.OUT)
-GPIO.output(in1,GPIO.LOW)
-GPIO.output(in2,GPIO.LOW)
-p=GPIO.PWM(en,1000)
+
 
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(in13,GPIO.OUT)
+GPIO.setup(in3,GPIO.OUT)
 GPIO.setup(in4,GPIO.OUT)
 GPIO.setup(en2,GPIO.OUT)
+
+GPIO.output(in1,GPIO.LOW)
+GPIO.output(in2,GPIO.LOW)
 GPIO.output(in3,GPIO.LOW)
 GPIO.output(in4,GPIO.LOW)
 p=GPIO.PWM(en,1000)
+p1 =GPIO.PWM(en2,1000)
 
 p.start(25)
+p1.start(25)
 print("\n")
 print("The default speed & direction of motor is LOW & Forward.....")
 print("r-run s-stop f-forward b-backward l-low m-medium h-high e-exit")
@@ -49,8 +52,8 @@ while(1):
         else:
          GPIO.output(in1,GPIO.LOW)
          GPIO.output(in2,GPIO.HIGH)
-         GPIO.output(in3,GPIO.HIGH)
-         GPIO.output(in4,GPIO.LOW)
+         GPIO.output(in3,GPIO.LOW)
+         GPIO.output(in4,GPIO.HIGH)
          print("backward")
          x='z'
 
@@ -59,7 +62,7 @@ while(1):
         print("stop")
         GPIO.output(in1,GPIO.LOW)
         GPIO.output(in2,GPIO.LOW)
-        GPIO.output(in3,GPIO.HIGH)
+        GPIO.output(in3,GPIO.LOW)
         GPIO.output(in4,GPIO.LOW)
         x='z'
 
@@ -76,24 +79,27 @@ while(1):
         print("backward")
         GPIO.output(in1,GPIO.LOW)
         GPIO.output(in2,GPIO.HIGH)
-        GPIO.output(in3,GPIO.HIGH)
-        GPIO.output(in4,GPIO.LOW)
+        GPIO.output(in3,GPIO.LOW)
+        GPIO.output(in4,GPIO.HIGH)
         temp1=0
         x='z'
 
     elif x=='l':
         print("low")
         p.ChangeDutyCycle(25)
+        p1.ChangeDutyCycle(25)
         x='z'
 
     elif x=='m':
         print("medium")
         p.ChangeDutyCycle(50)
+        p1.ChangeDutyCycle(50)
         x='z'
 
     elif x=='h':
         print("high")
         p.ChangeDutyCycle(100)
+        p1.ChangeDutyCycle(100)
         x='z'
      
     
